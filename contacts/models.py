9eb58ca1e59da5a69880d1cad675117ffcd50d26
Oleg Nyevyerov <onyevyerov@gmail.com>
2026-01-16 17:14:45 +0100

    write models ContactStatus, Contact in models.py

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactStatus(models.Model):
+    name = models.CharField(max_length=55, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Contact Statuses"
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=55)
+    phone_number = models.CharField(max_length=15, unique=True)
+    email = models.EmailField(null=True)
+    city = models.CharField(max_length=55)
+    status = models.ForeignKey(
+        ContactStatus, on_delete=models.PROTECT, related_name="contacts"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.phone_number}"
+
+    class Meta:
+        ordering = ["-created_at"]
