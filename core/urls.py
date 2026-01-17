e13f0086e6924dc41e9eb813075f91d74ab5e2b7
Oleg Nyevyerov <onyevyerov@gmail.com>
2026-01-17 22:09:42 +0100

    add url for contact_edit

--- core/urls.py
+++ core/urls.py
@@ -21,6 +21,7 @@ from contacts import views
 urlpatterns = [
     path('', views.contact_list, name='contact_list'),
     path("create", views.contact_create, name='contact_create'),
+    path("edit/<int:pk>", views.contact_edit, name='contact_edit'),
 ]
 
 app_name = "core"
