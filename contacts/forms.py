f2f24fb36188b84872a6642ea2851fbdc627c63c
Oleg Nyevyerov <onyevyerov@gmail.com>
2026-01-17 21:09:30 +0100

    write ContactForm in forms.py

---
+++ contacts/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from contacts.models import Contact
+
+
+from django import forms
+from .models import Contact
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'city', 'status']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter first name'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter last name'}),
+            'phone_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '+48 123 456 789'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'example@mail.com'}),
+            'city': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'e.g. Wroclaw'}),
+            'status': forms.Select(attrs={'class': 'form-select'}),
+        }
