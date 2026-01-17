c8cd5cc051abe3fbdde75bc9af69b48858a2f7b7
Oleg Nyevyerov <onyevyerov@gmail.com>
2026-01-17 21:11:00 +0100

    write contact_list view in views.py

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Contact
+
+
+def contact_list(request):
+    sort_by = request.GET.get('sort', '-created_at')
+
+    if sort_by not in ['last_name', '-last_name', 'created_at', '-created_at']:
+        sort_by = '-created_at'
+
+    contacts = Contact.objects.all().order_by(sort_by)
+
+    return render(request, "contact_list.html", {
+        'contacts': contacts,
+        'current_sort': sort_by
+    })
