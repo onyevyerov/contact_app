50400670715680b8ea60e1cd044131c9ee829158
Oleg Nyevyerov <onyevyerov@gmail.com>
2026-01-18 22:00:59 +0100

    write view for import data from csv file

--- contacts/views.py
+++ contacts/views.py
@@ -1,8 +1,12 @@
+import csv
+import io
+
+from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 
 from .forms import ContactForm
-from .models import Contact
+from .models import Contact, ContactStatus
 
 
 def contact_list(request):
@@ -53,3 +57,39 @@ def contact_delete(request, pk):
     contact = get_object_or_404(Contact, pk=pk)
     contact.delete()
     return redirect('contact_list')
+
+
+def import_contacts_csv(request):
+    if request.method == "POST":
+        csv_file = request.FILES.get('file')
+
+        if not csv_file or not csv_file.name.endswith('.csv'):
+            messages.error(request, 'Please upload the correct file .csv')
+            return redirect('contact_list')
+
+        try:
+            data_set = csv_file.read().decode('UTF-8')
+            io_string = io.StringIO(data_set)
+            next(io_string)
+
+            for row in csv.reader(io_string, delimiter=',', quotechar='"'):
+                # row: [first_name, last_name, phone, email, city, status_name]
+                status_obj, _ = ContactStatus.objects.get_or_create(name=row[5])
+
+                Contact.objects.update_or_create(
+                    email=row[3],
+                    defaults={
+                        'first_name': row[0],
+                        'last_name': row[1],
+                        'phone_number': row[2],
+                        'city': row[4],
+                        'status': status_obj
+                    }
+                )
+            messages.success(request, 'Import completed successfully!')
+        except Exception as e:
+            messages.error(request, f'Error while importing: {e}')
+
+        return redirect('contact_list')
+
+    return render(request, 'import_form.html')
