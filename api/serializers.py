236c50981addc431acd300aab1506e0e1aaed080
Oleg Nyevyerov <onyevyerov@gmail.com>
2026-01-18 22:24:20 +0100

    write ContactStatusSerializer, ContactSerializer in serializers.py

---
+++ api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from contacts.models import ContactStatus, Contact
+
+
+class ContactStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContactStatus
+        fields = ["id", "name"]
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    status = ContactStatusSerializer(read_only=True)
+    class Meta:
+        model = Contact
+        fields = ["id", "first_name", "last_name", "email", "phone_number", "city", "status", "created_at"]
